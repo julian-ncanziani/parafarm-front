d07753e76fe28379acdc59c5a54eb3316212e8b6
Julian <julian.noya@hotmail.com>
2024-09-15 00:32:20 -0300

    cambios

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -3,7 +3,7 @@
 import { signIn } from "next-auth/react";
 import { FcGoogle } from "react-icons/fc";
 
-export default async function SignIn() {
+export default function SignIn() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md text-center">
