343c13ed48b83ff41611b7e97fdc1d4a59d7ac9a
Julian <julian.noya@hotmail.com>
2024-09-18 09:53:23 -0300

    cambios en btn waw

--- components/WhatsAppBtn.tsx
+++ components/WhatsAppBtn.tsx
@@ -3,18 +3,18 @@ import { FaWhatsapp } from "react-icons/fa";
 const WhatsappBtn = () => {
 
     const text = 'Hello%20I%20have%20a%20question%20about%20your%20products';
-    const tel = '5491170617551'
+    const tel = '5491154186728';
 
     return(
         <>
             <a
-                href={`https://web.whatsapp.com/send?phone=${tel}&text=${text}`}
+                href={`https://wa.me/${tel}?text=${text}`}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="fixed bottom-4 right-4 bg-green-500 hover:bg-green-600 text-white font-normal py-2 px-2 rounded-full flex items-center shadow-lg"
+                className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded inline-flex items-center"
                 >
                 <FaWhatsapp className="w-5 h-5 mr-2" />
-                <span className="text-sm">Contactanos por WhatsApp</span> {/* Smaller text */}
+                Contact Us on WhatsApp
             </a>
         </>
     )
