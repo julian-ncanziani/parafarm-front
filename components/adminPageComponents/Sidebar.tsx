d4786dde1befae4d4d83ce5c57a547392e6e14ff
Julian <julian.noya@hotmail.com>
2024-09-18 10:18:55 -0300

    ultimos cambios

--- components/adminPageComponents/Sidebar.tsx
+++ components/adminPageComponents/Sidebar.tsx
@@ -9,6 +9,7 @@ import {
   DocumentTextIcon,
   ListBulletIcon,
 } from '@heroicons/react/24/outline';
+import {  FiArrowLeftCircle } from 'react-icons/fi'
 
 
 const Sidebar = () => {
@@ -31,52 +32,62 @@ const Sidebar = () => {
                 </div>
                 <nav className="flex-grow overflow-auto">
                 <ul className="space-y-2">
-                    <li>
-                    <button
-                        onClick={() => handleTabs('home')}
-                        className={`flex items-center p-4 w-full ${activeTab === 'home' ? 'bg-gray-700' : ''} whitespace-nowrap`}
-                    >
-                        <HomeIcon className="h-6 w-6" />
-                        {isSidebarOpen && <span className="ml-4">Home</span>}
-                    </button>
-                    </li>
-                    <li>
-                    <button
-                        onClick={() => handleTabs('products')}
-                        className={`flex items-center p-4 w-full ${activeTab === 'products' ? 'bg-gray-700' : ''} whitespace-nowrap`}
-                    >
-                        <ListBulletIcon className="h-6 w-6" />
-                        {isSidebarOpen && <span className="ml-4">Products</span>}
-                    </button>
-                    </li>
-                    <li>
-                    <button
-                        onClick={() => handleTabs('orders')}
-                        className={`flex items-center p-4 w-full ${activeTab === 'orders' ? 'bg-gray-700' : ''} whitespace-nowrap`}
-                    >
-                        <DocumentTextIcon className="h-6 w-6" />
-                        {isSidebarOpen && <span className="ml-4">Orders</span>}
-                    </button>
-                    </li>
-                    <li>
-                    <button
-                        onClick={() => handleTabs('users')}
-                        className={`flex items-center p-4 w-full ${activeTab === 'users' ? 'bg-gray-700' : ''} whitespace-nowrap`}
-                    >
-                        <UsersIcon className="h-6 w-6" />
-                        {isSidebarOpen && <span className="ml-4">Users</span>}
-                    </button>
-                    </li>
-                    <li>
-                    <button
-                        onClick={() => handleTabs('settings')}
-                        className={`flex items-center p-4 w-full ${activeTab === 'settings' ? 'bg-gray-700' : ''} whitespace-nowrap`}
-                    >
-                        <CogIcon className="h-6 w-6" />
-                        {isSidebarOpen && <span className="ml-4">Settings</span>}
-                    </button>
-                    </li>
-                </ul>
+    <li>
+        <button
+            onClick={() => handleTabs('home')}
+            className={`flex items-center p-4 w-full ${activeTab === 'home' ? 'bg-gray-700' : ''} whitespace-nowrap`}
+        >
+            <HomeIcon className="h-6 w-6" />
+            {isSidebarOpen && <span className="ml-4 hover:underline">Home</span>}
+        </button>
+    </li>
+    <li>
+        <button
+            onClick={() => handleTabs('products')}
+            className={`flex items-center p-4 w-full ${activeTab === 'products' ? 'bg-gray-700' : ''} whitespace-nowrap`}
+        >
+            <ListBulletIcon className="h-6 w-6" />
+            {isSidebarOpen && <span className="ml-4 hover:underline">Products</span>}
+        </button>
+    </li>
+    <li>
+        <button
+            onClick={() => handleTabs('orders')}
+            className={`flex items-center p-4 w-full ${activeTab === 'orders' ? 'bg-gray-700' : ''} whitespace-nowrap`}
+        >
+            <DocumentTextIcon className="h-6 w-6" />
+            {isSidebarOpen && <span className="ml-4 hover:underline">Orders</span>}
+        </button>
+    </li>
+    <li>
+        <button
+            onClick={() => handleTabs('users')}
+            className={`flex items-center p-4 w-full ${activeTab === 'users' ? 'bg-gray-700' : ''} whitespace-nowrap`}
+        >
+            <UsersIcon className="h-6 w-6" />
+            {isSidebarOpen && <span className="ml-4 hover:underline">Users</span>}
+        </button>
+    </li>
+    <li>
+        <button
+            onClick={() => handleTabs('settings')}
+            className={`flex items-center p-4 w-full ${activeTab === 'settings' ? 'bg-gray-700' : ''} whitespace-nowrap`}
+        >
+            <CogIcon className="h-6 w-6" />
+            {isSidebarOpen && <span className="ml-4 hover:underline">Settings</span>}
+        </button>
+    </li>
+    <li>
+        <a 
+            href="/" 
+            className={`flex items-center p-4 w-full whitespace-nowrap`} // Ajusta el estilo para coincidir con los otros botones
+        >
+            <FiArrowLeftCircle className="h-6 w-6" />
+            {isSidebarOpen && <span className="ml-4 hover:underline">Volver</span>}
+        </a>
+    </li>
+</ul>
+
                 </nav>
             </div>
         </div>
