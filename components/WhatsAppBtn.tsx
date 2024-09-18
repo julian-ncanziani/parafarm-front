a291b44c8bfd6b322c1dd58da82d1fc7dbd7fe2d
Julian <julian.noya@hotmail.com>
2024-09-18 09:59:39 -0300

    ultimos cambios waw btn

--- components/WhatsAppBtn.tsx
+++ components/WhatsAppBtn.tsx
@@ -3,7 +3,7 @@ import { FaWhatsapp } from "react-icons/fa";
 const WhatsappBtn = () => {
 
     const text = 'Hello%20I%20have%20a%20question%20about%20your%20products';
-    const tel = '5491154186728';
+    const tel = '5491170617551';
 
     return(
         <>
@@ -11,10 +11,10 @@ const WhatsappBtn = () => {
                 href={`https://wa.me/${tel}?text=${text}`}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded inline-flex items-center"
+                className="fixed bottom-4 right-4 bg-green-500 hover:bg-green-600 text-white py-2 px-3 rounded-full flex items-center shadow-lg"
                 >
                 <FaWhatsapp className="w-5 h-5 mr-2" />
-                Contact Us on WhatsApp
+                <span className="text-sm">Atencion al Cliente</span> {/* Smaller text */}
             </a>
         </>
     )
